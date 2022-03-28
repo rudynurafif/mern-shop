10c7458dba4da7c590548ad9a2d01193f86d7eab
rudynurafif <rudynurafif@gmail.com>
2022-03-29 01:20:17 +0700

    review

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -82,7 +82,7 @@ const ProductScreen = ({ history, match }) => {
                 </ListGroup.Item>
                 <ListGroup.Item>Price: ${product.price}</ListGroup.Item>
                 <ListGroup.Item>
-                  Description: ${product.description}
+                  Description: {product.description}
                 </ListGroup.Item>
               </ListGroup>
             </Col>
---
+++ note.md
@@ -0,0 +1,3 @@
+1. Tema aplikasi dibuat menggunakan Bootswatch (Lux Theme), terdapat banyak tema gratis yang bsia dipakai.
+2. Untuk style componentnya menggunakan react-bootstrap
+3. Icon yang digunakan diimport dari font-awesome menggunakan cdn
